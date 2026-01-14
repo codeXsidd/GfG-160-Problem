50e9ea5b8c07958e70a74a2c08ff6d9a5e3a8a35
Siddharth S <179855721+codeXsidd@users.noreply.github.com>
2026-01-14 17:45:19 +0530

    Add solution for minimum characters to add for palindrome

---
+++ String/19-Min Chars to Add for Palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    static int[] compute(String pat){
+        int n=pat.length();
+        int[] lps=new int[n];
+        lps[0]=0;
+        int len=0;
+        int i=1;
+        while(i<n){
+            if(pat.charAt(i)==pat.charAt(len))
+                lps[i++]=++len;
+                
+            else{
+                if(len!=0)
+                  len=lps[len-1];
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    public int minChar(String s) {
+        int n=s.length();
+        String rev=new StringBuilder(s).reverse().toString();
+        s=s+"$"+rev;
+        int[] lps=compute(s);
+        return (n-lps[lps.length-1]);
+        
+    }
+}
