97a8fbaa2540ed124aa44fa53a29d964e6d58383
Siddharth S <siddharth291206@gmail.com>
2026-03-16 21:41:45 +0530

    Create 32-K-th element of two Arrays.java

---
+++ Searching/32-K-th element of two Arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        int n=a.length,m=b.length;
+        int[] arr = new int[n+m];
+        int i=0,j=0,d=0;
+        while(i<n && j<m){
+            if(a[i]<b[j]){
+               arr[d++]=a[i++];}
+            else{
+              arr[d++]=b[j++];}
+        }
+        while(i<n){
+          arr[d++]=a[i++];
+        }
+        while(j<m){
+           arr[d++]=b[j++];
+        }
+        return arr[k-1];
+    }
+}
