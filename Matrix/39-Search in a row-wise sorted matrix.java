ecd98b64c6cc53f47182709f9c8ef367f74e73df
Siddharth S <siddharth291206@gmail.com>
2026-03-16 21:49:52 +0530

    Create 39-Search in a row-wise sorted matrix.java

---
+++ Matrix/39-Search in a row-wise sorted matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+   static boolean search(int[] arr,int x){
+       int lo=0,hi=arr.length-1;
+       while(lo<=hi){
+           int mid=(lo+hi)/2;
+           if(x==arr[mid])
+             return true;
+           if(x<arr[mid])
+             hi=mid-1;
+           else
+             lo=mid+1;
+       }
+       return false;
+   }
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        int n=mat.length,m=mat[0].length;
+        for(int i=0;i<n;i++){
+            if(search(mat[i],x))
+               return true;
+        }
+        return false;
+    }
+}
