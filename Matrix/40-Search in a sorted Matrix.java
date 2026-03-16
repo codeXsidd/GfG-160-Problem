52322902159f883076119e679983eadcf21d30ae
Siddharth S <siddharth291206@gmail.com>
2026-03-16 21:50:41 +0530

    Create 40-Search in a sorted Matrix.java

---
+++ Matrix/40-Search in a sorted Matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n=mat.length,m=mat[0].length;
+        int lo=0,hi=n*m-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(mat[row][col]==x)
+              return true;
+            if(mat[row][col]<x)
+              lo= mid+1;
+            else
+              hi=mid-1;
+        }
+        return false;
+    }
+}
