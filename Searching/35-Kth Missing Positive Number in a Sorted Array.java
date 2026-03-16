7f47684fbd3e26131c548fa6cb7326903e399332
Siddharth S <siddharth291206@gmail.com>
2026-03-16 21:44:19 +0530

    Create 35-Kth Missing Positive Number in a Sorted Array.java

---
+++ Searching/35-Kth Missing Positive Number in a Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int lo=0,hi=arr.length-1;
+        int res=arr.length+k;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>mid+k){
+                res=mid+k;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return res;
+    }
+}
