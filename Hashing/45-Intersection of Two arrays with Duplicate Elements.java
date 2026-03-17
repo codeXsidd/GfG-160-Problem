28a8a3bb978b9b8fc388f471699914a586e7579d
Siddharth S <siddharth291206@gmail.com>
2026-03-17 22:58:33 +0530

    Create 45-Intersection of Two arrays with Duplicate Elements.java

---
+++ Hashing/45-Intersection of Two arrays with Duplicate Elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ArrayList<Integer> intersect(int[] a, int[] b) {
+        // code here
+        HashSet<Integer> sa=new HashSet<>();
+        for(int num : a){
+            sa.add(num);
+        }
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int num : b){
+            if(sa.contains(num)){
+                res.add(num);
+                sa.remove(num);
+            }
+        }
+        return res;
+    }
+}
